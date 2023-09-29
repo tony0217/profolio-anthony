c3c595edeb12bc8279f929b90c5858dc2b4fc2b2
mac-anthony <mac-anthony@MacBook-Air-de-mac-anthony.local>
2023-09-29 18:13:33 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,63 @@
+'use client'
+
 import NextLink from "next/link";
 import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { GithubIcon, LinkedInIcon, RightArrowIcon } from "@/components/icons";
+import { Card, CardBody } from "@nextui-org/card";
+import { Button } from "@nextui-org/button";
+import { Avatar } from "@nextui-org/react";
+
+import Profile from '@/public/profile.jpeg'
 
 export default function Home() {
 	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-8 animate-fadeIn">
+			<Avatar isBordered color="warning" className="w-24 h-24 hover:scale-125 transition-transform duration-300 ease-in-out" src={Profile.src} />
+			<div className="inline-block max-w-lg text-center justify-center transform hover:scale-110 transition-transform duration-300 ease-in-out">
+				<h1 className={title({ color: "foreground", size: "lg" })}>Anthony Henriquez&nbsp;</h1>
 				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
+				<h2 className={subtitle({ class: "mt-4", color: 'yellow' })}>
+					Full-Stack Developer
 				</h2>
 			</div>
-
 			<div className="flex gap-3">
 				<Link
 					isExternal
 					as={NextLink}
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
+					href={siteConfig.links.linkedin}
+					className={`${buttonStyles({ color: "primary", radius: "full", variant: "shadow" })} transform hover:animate-bounce hover:scale-110 transition-transform duration-300 ease-in-out`}
 				>
-					Documentation
+					<LinkedInIcon size={18} />
+					LinkedIn
 				</Link>
 				<Link
 					isExternal
 					as={NextLink}
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
+					className={`${buttonStyles({ variant: "bordered", radius: "full" })} transform hover:animate-bounce hover:scale-110 transition-transform duration-300 ease-in-out`}
 					href={siteConfig.links.github}
 				>
 					<GithubIcon size={20} />
 					GitHub
 				</Link>
 			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
+			<div className="mt-8 transform hover:scale-105 transition-transform duration-300 ease-in-out">
+				<Card className="max-w-5xl p-6 rounded-lg shadow-lg hover:shadow-xl hover:bg-gradient-to-r hover:from-[#4D4D4D] hover:via-gray-400 hover:to-[#999999] hover:text-lg hover:text-black">
+					<CardBody className="text-sm md:text-md py-6 text-white transition-colors duration-300 ease-in-out hover:text-black">
+						Ingeniero de software especializado en desarrollo web, con enfoque en Node.js, JavaScript, TypeScript, Angular y React. Destaco en la creación de soluciones eficaces y aprendo rápidamente en entornos cambiantes. Puedo desempeñar varios roles, desde backend sólidos hasta interfaces frontend atractivas. Busco constantemente la excelencia técnica y la mejora continua.
+					</CardBody>
+				</Card>
+			</div>
+			<div className="flex mt-2 items-center">
+				<Button className="bg-gradient-to-r from-[#999999] to-[#4D4D4D] hover:from-[#4D4D4D] hover:to-[#999999] font-semibold py-2 px-4 rounded-full shadow-lg hover:shadow-xl transition-transform duration-300 transform hover:scale-125 hover:animate-pulse">
+					Go to Projects 
+					<RightArrowIcon/>
+				</Button>
 			</div>
 		</section>
 	);
 }
+
+
--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
+	name: "profolio - Anthony Henriquez",
 	description: "Make beautiful websites regardless of your design experience.",
 	navItems: [
 		{
@@ -34,36 +34,10 @@ export const siteConfig = {
 			label: "Dashboard",
 			href: "/dashboard",
 		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
+	
 	],
 	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui.org",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
+		github: "https://github.com/tony0217",
+		linkedin: "https://www.linkedin.com/in/anthony-henriquez-537391114/",
 	},
 };
